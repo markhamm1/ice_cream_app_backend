2476799abe951bb71641f938e38662dd05fed416
Mark <markandrewhamm@gmail.com>
2020-08-07 10:08:12 -0500

    index, show, create action

--- app/controllers/api/flavors_controller.rb
+++ app/controllers/api/flavors_controller.rb
@@ -1,2 +1,21 @@
 class Api::FlavorsController < ApplicationController
+  def index
+    @flavors = Flavor.all
+    render 'index.json.jb'
+  end
+
+  def show
+    @flavor = Flavor.find_by(id: params[:id])
+    render 'show.json.jb'
+  end
+  
+  def create
+    @flavor = Flavor.create(
+      name: params[:name],
+      ingredients: params[:ingredients],
+      color: params[:color],
+      image_url: params[:image_url]
+      )  
+    render 'show.json.jb'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   # get "/photos" => "photos#index"
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+  namespace :api do
+    get "/flavors" => "flavors#index"
+    post "/flavors" => "flavors#create"
+    get "/flavors/:id" => "flavors#show"
+  end
 end
